e558a91c7b4f9b578a603a2e2a45a49663ea7c55
jameskomo <james.komoh@gmail.com>
2019-02-22 09:18:21 +0300

    Carousel added and sliding properties defined. Interval function defined in JS

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// $('#landing-page').carousel({
+//      interval: 3000;
+//    })
+//  });
+
+$("#questions-2").on("click", function() {
+  event.preventDefault();
+  $("#set-1").hide();
+  $("#questions-2").hide();
+  $("#set-2").show();
+});
